a8512bc3951834f589d9c96c9933121758a2de37
Huy <huynhthanhhuy.ag667@gmail.com>
2026-04-08 20:33:17 +0700

    updateFrontend

---
+++ travel_smart/frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import FeaturedDestination from '../Components/FeaturedDestination'
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <FeaturedDestination />
+        </>
+    )
+}
+
+export default Home
