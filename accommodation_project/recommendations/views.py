eccc99e3df09f8153dd61fa8f823dbe6ecfbe802
congthanhtoine241 <thanhlaanh146@gmail.com>
2026-04-11 21:07:35 +0700

    Tối ưu hóa tiếng Việt và giữ nguyên thang điểm 5.0

--- accommodation_project/recommendations/views.py
+++ accommodation_project/recommendations/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from accommodations.models import Accommodation
 from preferences.models import UserPreference
-from .services import calculate_matching_score
+from .services import calculate_matching_score, get_candidate_accommodations
 
 def recommendation_result(request, pref_id):
     preference = get_object_or_404(UserPreference, id=pref_id)
 
-    # Lọc nhẹ để tải dữ liệu cơ bản
-    accommodations = Accommodation.objects.filter(capacity__gte=preference.guest_count)
+    accommodations = get_candidate_accommodations(preference)
 
-    # Gộp tên để tránh trùng lặp dữ liệu từ giả lập seed.py
-    unique_items = {}
-    for item in accommodations:
-        if item.name not in unique_items:
-            unique_items[item.name] = item
-            
-    valid_items = list(unique_items.values())
-    
     scored_results = []
 
-    for item in valid_items:
+    for item in accommodations:
         # Gọi thuật toán Matching Score. Rating mặc định của DB nằm sẵn trong item.rating
-        score = calculate_matching_score(item, preference)
-        scored_results.append((item, round(score, 2)))
+        scored_results.append((item, calculate_matching_score(item, preference)))
 
     # Sắp xếp từ cao xuống thấp theo Score
     scored_results.sort(key=lambda x: x[1], reverse=True)
