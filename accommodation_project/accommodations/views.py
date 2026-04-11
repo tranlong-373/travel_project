ebaa0ef9a477b1c3241bc95519af35616c7f0ce5
hohuunghiem <nghiemho.040106@gmail.com>
2026-04-11 17:00:14 +0700

    Pull from Cong Thanh add maKS , count ,..update review and favourite features

--- accommodation_project/accommodations/views.py
+++ accommodation_project/accommodations/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Accommodation
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Accommodation, AccommodationReview
 
 def accommodation_list(request):
     accommodations = Accommodation.objects.all()
@@ -7,8 +8,32 @@ def accommodation_list(request):
         'accommodations': accommodations
     })
 
+
 def accommodation_detail(request, pk):
     accommodation = get_object_or_404(Accommodation, pk=pk)
+    approved_reviews = AccommodationReview.objects.filter(
+        accommodation=accommodation,
+        is_approved=True
+    ).order_by('-created_at')
+
     return render(request, 'accommodations/accommodation_detail.html', {
-        'accommodation': accommodation
+        'accommodation': accommodation,
+        'approved_reviews': approved_reviews
     })
+@login_required
+def add_review(request, accommodation_id):
+    accommodation = get_object_or_404(Accommodation, id=accommodation_id)
+
+    if request.method == 'POST':
+        score = int(request.POST.get('score'))
+        comment = request.POST.get('comment')
+
+        AccommodationReview.objects.create(
+            user=request.user,
+            accommodation=accommodation,
+            score=score,
+            comment=comment,
+            is_approved=False
+        )
+
+    return redirect('accommodation_detail', pk=accommodation.id)
