c20b247cfdeed7cae6bf3d3d64bbcb5a15d11e15
TOMSU\congi <minh.b.chu@tgl-sol.com>
2026-04-11 15:09:45 +0700

    update rating

--- accommodation_project/accommodations/models.py
+++ accommodation_project/accommodations/models.py
@@ -14,10 +14,17 @@ class Accommodation(models.Model):
     address = models.CharField(max_length=255)
     price_per_night = models.IntegerField()
     capacity = models.IntegerField()
+
     rating = models.FloatField(default=0)
+    rate_count = models.IntegerField(default=0)
+
     amenities = models.JSONField(default=list, blank=True)
     description = models.TextField(blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
     def __str__(self):
         return self.name
+    def update_rating(self, new_rating):
+        self.rating = (self.rating * self.rate_count)/(self.rate_count+1) + new_rating/(self.rate_count+1)
+        self.rate_count = self.rate_count + 1
+        self.save()
